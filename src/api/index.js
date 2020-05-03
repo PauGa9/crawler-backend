44e6220015c655252146d249076bf3e80b439812
Pau Gasol <gasanpau@gmail.com>
2020-05-03 13:07:22 +0200

    move publisher.js to a rabbitmq folder

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT;
 
-const createPublisher = require('./publisher.js');
+const createPublisher = require('../rabbitmq/publisher');
 const createMongoRepository = require('../mongodb');
 
 Promise.all([createPublisher, createMongoRepository]).then(function(promises) {
