df24f715da7a506f2944083548f88558a2e3fbaf
Pau Gasol <gasanpau@gmail.com>
2020-05-03 13:00:01 +0200

    add basic mongo repository

---
+++ src/mongodb/index.js
@@ -0,0 +1,28 @@
+const MongoClient = require("mongodb").MongoClient;
+const COLLECTION_NAME = 'visited_pages';
+
+const url = `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}`;
+const dbName = 'crawler';
+const client = new MongoClient(url, { useNewUrlParser: true });
+
+function createDB() {
+    return new Promise((resolve, reject) => {
+        client.connect(function(error) {
+            if (error) return reject(error);
+        
+            const db = client.db(dbName);
+            resolve(createRepository(db));
+        })
+    })
+}
+
+function createRepository(db) {
+    return {
+        findPagesByMainPage: function (mainPage) {
+            const collection = db.collection(COLLECTION_NAME);
+            return collection.find({"mainPage": mainPage}).toArray();
+        }
+    }
+}
+
+module.exports = createDB()
